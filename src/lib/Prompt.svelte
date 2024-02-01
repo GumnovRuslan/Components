bca9f631933957dff2d3651db0bcff37702a9ff4
Gumnov Ruslan <ruslang.crocode@gmail.com>
2024-02-01 18:00:28 +0300

    fix component prompt

--- src/lib/Prompt.svelte
+++ src/lib/Prompt.svelte
@@ -1,44 +1,54 @@
 <script>
 	import { onMount } from 'svelte';
 
+	let buttons; //only elements with data-tooltip
 	let tooltipElem;
-
 	onMount(() => {
-		document.onmouseover = function (e) {
-			let target = e.target;
-			let tooltipHtml = target.dataset.tooltip;
-			if (!tooltipHtml) return;
-			tooltipElem = document.createElement('div');
-			tooltipElem.className = 'tooltip';
-			tooltipElem.innerHTML = tooltipHtml;
-			document.body.append(tooltipElem);
-			tooltipElem.style.cssText = `
-                position: fixed;
-                padding: 15px;
-                border: 2px solid #b9b9b9;
-                border-radius: 4px;
-                text-align: center;
-                background: #f5f5f5;
-            `;
+		buttons = document.querySelectorAll('button[data-tooltip]');
+		buttons.forEach((button) => setTooltip(button));
+	});
 
-			let coords = target.getBoundingClientRect();
-			let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+	function setTooltip(button) {
+		button.addEventListener('mouseover', openTooltip);
+		button.addEventListener('mouseout', closeTooltip);
+
+		function openTooltip() {
+			let tooltipText = button.dataset.tooltip;
+			if (!tooltipText) return;
+			else createTooltip(tooltipText);
+
+			const coords = button.getBoundingClientRect();
+			let left = coords.left + (button.offsetWidth - tooltipElem.offsetWidth) / 2;
 			if (left < 0) left = 0;
 
 			let top = coords.top - tooltipElem.offsetHeight - 5;
-			if (top < 0) top = coords.top + target.offsetHeight + 5;
+			if (top < 0) top = coords.top + button.offsetHeight + 5;
 
 			tooltipElem.style.left = left + 'px';
 			tooltipElem.style.top = top + 'px';
-		};
+		}
 
-		document.onmouseout = function (e) {
-			if (tooltipElem) {
-				tooltipElem.remove();
-				tooltipElem = hull;
-			}
-		};
-	});
+		function closeTooltip() {
+			if (tooltipElem) tooltipElem.remove();
+		}
+
+		function createTooltip(text) {
+			tooltipElem = document.createElement('div');
+			tooltipElem.className = 'tooltip';
+			tooltipElem.innerHTML = text;
+			button.append(tooltipElem);
+			// tooltipElem.style.position = 'fixed';
+			tooltipElem.style.cssText = `
+					position: fixed;
+					padding: 15px;
+					border: 2px solid #b9b9b9;
+					border-radius: 4px;
+					text-align: center;
+					background: #f5f5f5;
+				`;
+		}
+	}
+	// $: if (tooltipElem) tooltipElem.classList.add('tooltip');
 </script>
 
 <p>
@@ -59,9 +69,14 @@
 
 <button class="btn" data-tooltip="длинный текст с подсказкой">Кнопка 1</button>
 <button class="btn" data-tooltip="Подсказка с <br> HTML тегами">Кнопка 2</button>
+<button class="btn" data-tooltip="0">Кнопка 3</button>
 
 <style>
 	.btn {
+		padding: 5px 20px;
+		background: #d9cffa;
+		border: 1px solid #000;
+		border-radius: 4px;
 	}
 	.tooltip {
 		position: fixed;
